61ad10a76f83d02a59e5987fdd772733a76d6e6f
jason2714 <jason27146913@gmail.com>
2022-10-23 19:32:07 +0800

    Initial Commit

---
+++ E_Book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  E_Book
+//
+//  Created by User21 on 2022/10/23.
+//  Copyright © 2022 jacky. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
